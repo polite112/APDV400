1476d949ee2a5dbb496abd48ec9c3e69897ef3e0
MjBembe <61251576+MjBembe@users.noreply.github.com>
2020-07-13 12:35:19 +0200

    Update RecursionDemo.java

--- RecursionDemo.java
+++ RecursionDemo.java
@@ -1,9 +1,8 @@
 
 import java.util.Scanner;
 
-
-    public static void main(String[] args)
-    {
+public class RecursionDemo{
+    public static void main(String[] args)    {
          System.out.println("Enter an integer:");
          Scanner keyboard = new Scanner(System.in);;
          int number = keyboard.nextInt( );
@@ -31,7 +30,7 @@ import java.util.Scanner;
             displayAsWords(number / 10);
             System.out.print(getWordFromDigit(number % 10) + " ");
         }
-	}
+    }
 	
     /**
      Precondition: 0 <= digit <= 9
